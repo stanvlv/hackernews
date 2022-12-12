2be20c613a7b8a59ae19cf06b6496ba521dac972
Ana Rosa <asofiamcarvalho@gmail.com>
2022-12-12 16:39:42 +0100

    first draft

--- group5news/src/App.js
+++ group5news/src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
 import data from './hackernews.json'
+import Top from "./Components/Top"
+import Main from "./Components/Main"
+import Footer from "./Components/Footer"
 
 function App() {
   return (
     <div className="App">
-      
+      <Top />
+      <Main />
+      <Footer />
     </div>
   );
 }
