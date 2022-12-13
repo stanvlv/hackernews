3030a661d4f49b1c94d15088483c527124aed85e
stanvlv <114359328+stanvlv@users.noreply.github.com>
2022-12-13 16:48:47 +0100

    first commit

--- group5news/src/Components/Main.js
+++ group5news/src/Components/Main.js
@@ -1,7 +1,33 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+
+import axios from 'axios';
+
 
 export default function Main() {
+
+
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+      axios.get('http://hn.algolia.com/api/v1/search?tags=front_page')
+      .then(response => setData(response.data.hits))
+      .catch(error => console.log(error))
+    }, [])
+  console.log(data)
   return (
-    <div>Main</div>
+    <div>
+      {
+        data?.map((data) => {
+          return (
+            <div>
+              <h3>{data.title}</h3>
+              ({data.url})
+            </div>
+          )
+        })
+      }
+
+       </div>
   )
 }
+
