bc225e88f99fdaa084b0418d4a33813e74c9721d
Ana Rosa <asofiamcarvalho@gmail.com>
2022-12-14 15:42:45 +0100

    Pagination added

--- group5news/src/Components/MainNews.js
+++ group5news/src/Components/MainNews.js
@@ -1,13 +1,25 @@
+import ReactPaginate from 'react-paginate';
+import React, {useState, useEffect} from 'react'
 export default function MainNews({data}) {
 
+   //Start of pagination:
+   const [itemOffset, setItemOffset] = useState(0);
+   const itemsPerPage = 6;
+   const endOffset = itemOffset + itemsPerPage;
+   const currentItems = data.slice(itemOffset, endOffset);
+   const pageCount = Math.ceil(data.length / itemsPerPage);
+ 
+   const handlePageClick = (event) => {
+     const newOffset = (event.selected * itemsPerPage) % data.length;
+  
+     setItemOffset(newOffset);
+   };
    
 
-    
-
     return (
-
+<div>
         <ul>
-        {   data?.map((item) => {
+        {   currentItems?.map((item) => {
                 return (
                     <li className='story' key={item.objectID} >
                     <p id='firstP'><b>{item.title}</b> (<a href={item.url} target="_blank">{item.url}</a>)</p>
@@ -17,5 +29,22 @@ export default function MainNews({data}) {
     })
   }
   </ul>
+  <ReactPaginate
+          breakLabel="..."
+          nextLabel="next >"
+          onPageChange={handlePageClick}
+          pageRangeDisplayed={3}
+          pageCount={pageCount}
+          previousLabel="< previous"
+          renderOnZeroPageCount={null}
+          containerClassName="pagination"
+          pageLinkClassName='page-num'
+          previousLinkClassName='page-num'
+          nextLinkClassName='page-num'
+          activeLinkClassName='active'
+        />
+
+  </div>
+  
     )
 }
