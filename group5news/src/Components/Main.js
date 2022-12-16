49b82b012a80d08b29dcac54e0080ea3a34660f9
stanvlv <114359328+stanvlv@users.noreply.github.com>
2022-12-16 14:55:57 +0100

    last changes

--- group5news/src/Components/Main.js
+++ group5news/src/Components/Main.js
@@ -3,6 +3,10 @@ import React, {useState, useEffect} from 'react'
 import axios from 'axios';
 import MainNews from './MainNews';
 import Footer from './Footer'
+
+import { usePromiseTracker, trackPromise } from "react-promise-tracker"
+
+
 export default function Main() {
 
   // this query is the last part of the APi link which is default with the home page
@@ -13,9 +17,19 @@ export default function Main() {
 
 
 
+    const LoadingIndicator = props => {
+      const { promiseInProgress } = usePromiseTracker();
+         return (
+          promiseInProgress && 
+          <img src='https://media4.giphy.com/media/hWZBZjMMuMl7sWe0x8/200.webp?cid=ecf05e47wpb5o584qnk2fdfgsqq7ol7fjz4nvxr5co5lgoh7&rid=200.webp&ct=g'  />
+        );  
+       }
+
+
     //here we get API with useState at the end because we change it with our search from Footer
     useEffect(() => {
-      axios.get(`http://hn.algolia.com/api/v1/search?${query}`)
+      trackPromise(
+      axios.get(`http://hn.algolia.com/api/v1/search?${query}`))
       .then(response => setData(response.data.hits))
       .catch(error => alert("Sorry! :( We have an error: " + error))
     }, [query])
@@ -28,6 +42,7 @@ export default function Main() {
      <MainNews data={data}  />
      {/* here we make possible to use the setquery in the footer so we can change it there
      with the search value */}
+     <LoadingIndicator/>
      <Footer setQuery={setQuery} />
        </div>
   )
