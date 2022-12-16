34535e9f289f1441b25dc8c359fd473e07518a3a
Jannis-web <118267813+Jannis-web@users.noreply.github.com>
2022-12-16 09:50:12 +0100

    loading feature

---
+++ group5news/src/Components/loader.js
@@ -0,0 +1,8 @@
+window.addEventListener( load, () => {
+    const loader = document.querySelector(".loader");
+
+    loader.classList.add("loader-hidden");
+    loader.addEventListener("transisioned", () => {
+        document.body.removeChild("loader")
+    })
+})
